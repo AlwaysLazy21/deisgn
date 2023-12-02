cd26d9337654b96c84a08f1237369720deb15d32
AlwaysLazy21 <918382809@qq.com>
2023-12-02 22:31:19 +0800

    fix sersor.py

--- sersor.py
+++ sersor.py
@@ -35,11 +35,10 @@ def receive_serial_data(port, baud_rate):
                         continue
                     sensor_data = json.loads(data)
 
-                    if not (20 <= sensor_data["temperature"] <= 32 and 0.2 <= sensor_data["humidity"] <= 0.8):
+                    if not (20 <= sensor_data["temperature"] <= 30 and 0.2 <= sensor_data["humidity"] <= 0.8):
                         print("异常数据！触发报警。")
                         print(f"异常数据内容：{sensor_data}")  # 输出异常数据到控制台
                         winsound.Beep(1000, 2000)
-
                         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                         with open('sensor_log.txt', 'a', encoding='utf-8') as log_file:
                             log_file.write(
@@ -52,9 +51,9 @@ def receive_serial_data(port, baud_rate):
                             log_file.write(f"{timestamp}: {sensor_data}\n")
 
                 except json.JSONDecodeError as e:
-                    print(f"JSON解析错误：{e}")
-                except e:
-                    pass
+                    log_file.write(f"{timestamp}: {data}\n")
+                except Exception:
+                    log_file.write(f"{timestamp}: {Exception}\n")
             else:
                 # 没有接收到信息，向串口发送当前时间
                 send_current_time(ser)
@@ -69,5 +68,5 @@ def receive_serial_data(port, baud_rate):
 if __name__ == "__main__":
     list_available_ports()  # 显示可用的串口
     serial_port = input("请输入串口端口（例如，COM1或/dev/ttyUSB0）：")
-    baud_rate = 9600
+    baud_rate = 115200
     receive_serial_data(serial_port, baud_rate)
